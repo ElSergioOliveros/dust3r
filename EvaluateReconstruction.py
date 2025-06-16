6b023246c86f543e17464a2f7664b1d742842c60
Sergio Oliveros <s.oliverosb@uniandes.edu.co>
2025-06-16 04:52:45 +0000

    Stuff

--- EvaluateReconstruction.py
+++ EvaluateReconstruction.py
@@ -5,10 +5,11 @@ from dust3r.image_pairs import make_pairs
 from dust3r.cloud_opt import global_aligner, GlobalAlignerMode
 from dust3r.demo import get_3D_model_from_scene
 import PIL.Image
+import pandas as pd
+import numpy as np
 
 if __name__ == '__main__':
     device = 'cuda'
-    batch_size = 1
     schedule = 'cosine'
     lr = 0.01
     niter = 300
@@ -17,14 +18,17 @@ if __name__ == '__main__':
     # you can put the path to a local checkpoint in model_name if needed
     model = AsymmetricCroCo3DStereo.from_pretrained(model_name).to(device)
     # load_images can take a list of images or a directory
-    images = load_images_from_PIL([PIL.Image.open('croco/assets/Chateau1.png'), PIL.Image.open('croco/assets/Chateau2.png')], size=512)
-    print(images)
-    pairs = make_pairs(images, scene_graph='complete', prefilter=None, symmetrize=True)
-    output = inference(pairs, model, device, batch_size=batch_size)
 
+    imageDf = pd.read_csv("/home/soliverosb/dataFor3D/carScene.csv")
+    n = 2
+    PILImages = [ PIL.Image.open(imageDf['paths'].iloc[i]) for i in range(n) ]
+    transformationMatrices = [np.array(imageDf['rotation_matrices'].iloc[i]) for i in range(n)]
 
-    view1, pred1 = output['view1'], output['pred1']
-    view2, pred2 = output['view2'], output['pred2']
+    images = load_images_from_PIL(PILImages, size=512)
+
+    pairs = make_pairs(images, scene_graph='complete', prefilter='cyc20', symmetrize=True)
+    output = inference(pairs, model, device)
+
 
     scene = global_aligner(output, device=device, mode=GlobalAlignerMode.PointCloudOptimizer)
     loss = scene.compute_global_alignment(init="mst", niter=niter, schedule=schedule, lr=lr)
@@ -35,6 +39,28 @@ if __name__ == '__main__':
     poses = scene.get_im_poses()
     pts3d = scene.get_pts3d()
     confidence_masks = scene.get_masks()
+
+
+    for i in range(1, len(poses)):
+        homoMat0 = poses[i-1].cpu().detach().numpy()
+        homoMat1 = poses[i].cpu().detach().numpy()
+        
+        
+        rotationMat0 = homoMat0[:3, :3]
+        transVect0 = homoMat0[:3, 3]
+
+        rotationMat1 = homoMat1[:3, :3]
+        transVect1 = homoMat1[:3, 3]
+
+        relativeRotationAccuracy = np.arccos((np.trace(rotationMat0@rotationMat1.T) - 1)/2)
+
+        r = np.arccos((np.trace(transformationMatrices[0]@transformationMatrices[1].T) - 1)/2)
+
+        print(2)
+
+     
+        
+
 
     # visualize reconstruction
     get_3D_model_from_scene("./", True, scene)
